55f39d1cbd5e675c30e030d45f7e6fb28ef0790d
the-darklord <saimihirnath@protonmail.com>
2024-03-22 01:28:51 +0530

    Updated v1

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import React, { Component } from "react";
 
 class App extends Component {
     state = {
-        counters: [
-            { id: 1, value: 4 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
-        ],
+        counters: [],
     };
 
     handleIncrement = (counter) => {
